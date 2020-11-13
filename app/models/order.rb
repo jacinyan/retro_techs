a815b1742706d8a1805f77b2fdf83a8809ab42bf
Jacin <jacinjiyan@gmail.com>
2020-11-13 11:25:57 +1100

    it fails, ready to try again

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,10 @@
 class Order < ApplicationRecord
   belongs_to :user
 
-  has_many :order_items
-  has_many :items, through: :order_items
+  has_many :order_items, dependent: :destroy
+  has_many :items, through: :order_items, source: :item
 
   validates :total, presence: true
   validates_numericality_of :total, :greater_than_zero => 0
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   devise_for :users
-  resources :items
-  resources :orders, :only => [:index, :show, :create]
+  resources :items 
+  resources :orders, :only => [:index, :show, :new, :create] 
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
